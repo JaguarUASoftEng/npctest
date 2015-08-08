70e0faad239fcd6d180889d23270d012211c181b
Reid Phillips <xorlogic@gmail.com>
2015-08-08 17:46:10 -0500

    Deregister JDBC drivers on context destroyed

--- src/org/npc/test/TestServletContextListener.java
+++ src/org/npc/test/TestServletContextListener.java
@@ -1,5 +1,10 @@
 package org.npc.test;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -8,18 +13,20 @@ public class TestServletContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent arg0) {}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {}
+	public void contextDestroyed(ServletContextEvent arg0) {
+		this.deregisterDrivers();
+	}
 	
-//	private void deregisterDrivers() {
-//		ClassLoader cl = Thread.currentThread().getContextClassLoader();
-//		Enumeration<Driver> drivers = DriverManager.getDrivers();
-//		for (; drivers.hasMoreElements(); ) {
-//			Driver driver = drivers.nextElement();
-//			if (driver.getClass().getClassLoader() == cl) {
-//				try {
-//					DriverManager.deregisterDriver(driver);
-//				} catch (SQLException ex) {}
-//			}
-//		}
-//	}
+	private void deregisterDrivers() {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		for (; drivers.hasMoreElements(); ) {
+			Driver driver = drivers.nextElement();
+			if (driver.getClass().getClassLoader() == cl) {
+				try {
+					DriverManager.deregisterDriver(driver);
+				} catch (SQLException ex) {}
+			}
+		}
+	}
 }
